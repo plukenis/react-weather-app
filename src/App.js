160ad033cd6644c0680d90dd823979abd638c402
plukenis <ieva.zaboole@gmail.com>
2021-10-04 14:02:42 +0300

    last update

--- src/App.js
+++ src/App.js
@@ -60,8 +60,8 @@ function App() {
           </div>
           <div className = "weather-box">
            <div className = "temp">{Math.round(weather.main.temp)}°c</div>
+           <div className = "weather">{weather.weather[0].main}</div>
           </div>
-          <div className = "weather">{weather.weather[0].main}</div>
         </div>
         ) : ('')}
       </main>
